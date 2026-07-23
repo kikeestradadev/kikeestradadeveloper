c58cb22acefefabdd7dfbce1d60e060aac59a294
kikeestradadev <125921116+kikeestradadev@users.noreply.github.com>
2026-07-23 13:49:06 -0600

    liompiar

--- README.md
+++
@@ -1,5 +0,0 @@
-# Pug Email Boilerplate 
-
-this is an Email (pug) Boilerplate developed by me to develop Email templates with different practices and standarts
-
-## gh-pages link: https://kikeestradadev.github.io/kikeestradadeveloper/
--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-import coreModule from './modules/coreModule';
-import internalModule from './modules/internalModule';
-
-const initComponents = () => {
-	coreModule();
-	internalModule();
-};
-
-document.addEventListener('DOMContentLoaded', initComponents);
